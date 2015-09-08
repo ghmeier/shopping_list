bd9709edda3c1ea8dd81fe941b4bc3d2b0b00ec5
Garret Meier <meier.garret@gmail.com>
2015-09-08 12:23:32 -0500

    connecting to firebase

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var express = require("express");
+var Firebase = require("firebase");
+var fb_root = new Firebase("http://chillpad-shopping.firebaseio.com");
 var app = express();
 
 app.set("port",(process.env.PORT || 5000));
 
 app.get("/ping",function(req,res){
 	res.send("pong");
+});
+
+app.get("/",function(req,res){
+	res.json({message:null});
+});
+
+app.get("/list",function(req,res){
+	var list = fb_root.child("list");
+
+	list.once("value",function(snap){
+		res.json(snap.val());
+	},function(err){
+		res.json({message:"Error retrieving list.",err:err});
+	});
 });
 
 var server = app.listen(app.get("port"),function(){
